f5f3d88576d5a5babf1d9614ebe423215bfb3b44
MACBOOK 01 <macbook01@Kriti-Sinha-macbook.local>
2026-02-02 19:12:56 +0530

    Task3 Added

---
+++ om/Task3/HomeScreen.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+ const product = {
+    id: 1,
+    name: 'I Phone',
+    description: 'This is a Phone'
+  }
+
+export default HomeScreen = ({navigation}) =>{
+
+  return(
+    <View style = {styles.container}>
+      
+      <Text style = {styles.text}>This is Home Page</Text>
+      <Button title = "Go To Prroduct Page"
+      onPress = {() =>{
+        navigation.navigate('ProductDetails' , {productData:product})
+      }}/>
+    
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000000'
+  },
+  text:{
+    color: 'white',
+    marginBottom: 10,
+  }
+})
